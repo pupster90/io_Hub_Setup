2a705ce3ee1a8d645e3c1ea058bdf59da549b49f
pupster90 <m3elliot@eng.ucsd.edu>
2018-12-06 22:37:02 -0500

    removed ssd storage class, will try again later

--- Start_io_Hub.sh
+++ Start_io_Hub.sh
@@ -3,11 +3,10 @@
 ###
 
 ### Overview
-# At the time of this tutorial I used helm versions 2.11 and jupyterhub version 0.8-774d36f
+# At the time of this tutorial I used helm versions 2.11 and jupyterhub version 0.7.0
 # Helm Releases: https://github.com/helm/helm/releases
 # JupyterHub Releases: https://jupyterhub.github.io/helm-chart/
 # Helm Tutorial: https://docs.helm.sh/using_helm/#quickstart-guide
-JUPYTER_VERSION=0.8-ccc1e6b
 
 ### Download & Set Up Helm
 # https://zero-to-jupyterhub.readthedocs.io/en/latest/setup-helm.html
@@ -18,7 +17,8 @@ helm init --service-account tiller
 kubectl patch deployment tiller-deploy --namespace=kube-system --type=json --patch='[{"op": "add", "path": "/spec/template/spec/containers/0/command", "value": ["/tiller", "--listen=localhost:44134"]}]'
 
 # Make io Hub faster, Create Optimized Storage Class
-kubectl apply -f storageclass.yaml
+# NOTE: I couldn't get this working, will try again later
+#kubectl apply -f storageclass.yaml
 
 ### Create & Run JupyterHub
 # https://zero-to-jupyterhub.readthedocs.io/en/latest/setup-jupyterhub.html
@@ -27,8 +27,6 @@ sed -i 's/<RANDOM_HEX>/'"$( openssl rand -hex 32 )"'/g' config.yaml
 # Add jupyterhub github repo to helm
 helm repo add jupyterhub https://jupyterhub.github.io/helm-chart/
 helm repo update
-# Run the config.yaml file to start up JupyterHub
-RELEASE=jhub
-NAMESPACE=jhub
-helm upgrade --install $RELEASE jupyterhub/jupyterhub --namespace $NAMESPACE --version $JUPYTER_VERSION --values config.yaml
+# Run the config.yaml file to start up JupyterHub: RELEASE=NAMESPACE=jhub, JUPYTER_VERSION=0.7.0
+helm upgrade --install jhub jupyterhub/jupyterhub --namespace jhub --version 0.8-ccc1e6b --values config.yaml
 
--- Update_io_Hub.sh
+++ Update_io_Hub.sh
@@ -1,2 +1,2 @@
 # After JupyterHub has started up, run this command to update it settings according to the config.yaml file
-helm upgrade $RELEASE jupyterhub/jupyterhub --version=jupyterhub-0.8-ccc1e6b --values config.yaml
+helm upgrade jhub jupyterhub/jupyterhub --version=0.8-ccc1e6b --values config.yaml
